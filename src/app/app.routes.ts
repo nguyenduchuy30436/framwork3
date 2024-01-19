7dda314c3a3d57528055cfc1e3ac2668d2b3a9cd
nguyenduchuy30436 <huyndph30436@fpt.edu.vn>
2024-01-19 22:12:47 +0700

    add,delete,edit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import {} from './pages/'
+export const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+
+  { path: 'admin', component: HomeComponent },
+
+];
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core'; 
+import { HttpClient } from '@angular/common/http';  
+import { Product } from '../types/Product';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+ 
+  apiUrl = 'https://fakestoreapi.com/products'; 
+  http = inject(HttpClient); 
+  constructor() {}
+
+  getProductList(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl); 
+  }
+}
